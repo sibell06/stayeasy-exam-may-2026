ac0fd3ad2ed8247a506654a6ef3449a5b67dac70
sibell06 <sibel.hodzhova@abv.bg>
2026-06-13 16:40:54 +0300

    feat: add validation annotations to RegisterBindingModel

--- src/main/java/com/softuni/stayeasy/model/dto/user/RegisterBindingModel.java
+++ src/main/java/com/softuni/stayeasy/model/dto/user/RegisterBindingModel.java
@@ -1,12 +1,32 @@
 package com.softuni.stayeasy.model.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class RegisterBindingModel {
 
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
     private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 30, message = "Password must be between 6 and 30 characters")
     private String password;
+
+    @NotBlank(message = "Please confirm your password")
     private String confirmPassword;
+
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, max = 30, message = "First name must be between 2 and 30 characters")
     private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, max = 30, message = "Last name must be between 2 and 30 characters")
     private String lastName;
 
     public String getUsername() {
@@ -45,4 +65,3 @@ public class RegisterBindingModel {
     public void setLastName(String lastName) {
         this.lastName = lastName; }
 }
-
